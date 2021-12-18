c32b07d9023456e652a6df1f90cfbb19b575d225
Arun Jayaraman <riparun.aj@gmail.com>
2021-12-18 02:42:56 -0500

    Router test 3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import IPage from "../interfaces/page";
 const HomePage: React.FunctionComponent<IPage> = props => {
     return (
         <div>
-            <p>HOME PAGE Published V2</p>
+            <p>HOME PAGE Published V3</p>
             <a href = "/arunjayaraman95.github.io/schedule">Schedule</a>
         </div>
     )
